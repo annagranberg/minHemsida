aea9387f51a033dbe7225851708e5448b74ccabc
annagranberg <annagranberg03@gmail.com>
2025-04-08 16:16:40 +0200

    update

--- js.js
+++ js.js
@@ -1,3 +1,13 @@
-document.getElementById("portfolio").addEventListener("click", function () {
-    window.location.href = "/portfolio";
-});
+function switchLanguage(lang) {
+    if (lang === 'sv') {
+        document.getElementById('left-sv').style.display = 'block';
+        document.getElementById('right-sv').style.display = 'block';
+        document.getElementById('left-en').style.display = 'none';
+        document.getElementById('right-en').style.display = 'none';
+    } else if (lang === 'en') {
+        document.getElementById('left-sv').style.display = 'none';
+        document.getElementById('right-sv').style.display = 'none';
+        document.getElementById('left-en').style.display = 'block';
+        document.getElementById('right-en').style.display = 'block';
+    }
+}
